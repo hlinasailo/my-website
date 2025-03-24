b5e29eb1e3691c5ecf88a2434b1d857bd7556097
hlinasailo <144549088+hlinasailo@users.noreply.github.com>
2025-03-24 16:47:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/my-website/", // Change this to match your GitHub repo name
   plugins: [react()],
-  base: '/my-website/'
-})
+});
